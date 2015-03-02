ea49d0bb1248904bb725cd91006306aee9ae7bb0
Tag <tagadvance@gmail.com>
2015-03-02 06:03:31 -0800

    test stubs

---
+++ test/technology/tag/codejam/base/DefaultInputSourceTest.java
@@ -0,0 +1,19 @@
+package technology.tag.codejam.base;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DefaultInputSourceTest {
+
+	@Test
+	public void testDefaultInputSource() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetInputStream() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/technology/tag/codejam/base/DefaultOutputDestinationTest.java
@@ -0,0 +1,19 @@
+package technology.tag.codejam.base;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DefaultOutputDestinationTest {
+
+	@Test
+	public void testDefaultOutputDestination() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetOutputStream() {
+		fail("Not yet implemented");
+	}
+
+}
